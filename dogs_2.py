37b273da88602f8fc1e0907d41ce61f12796b566
tatanakondakova <tnea@mail.ru>
2024-09-08 06:42:59 +0800

    Вместо кнопки добавлено меню

--- dogs_2.py
+++ dogs_2.py
@@ -12,6 +12,8 @@ def load_image(url):
         response.raise_for_status() # для обработки исключений
         image_data = BytesIO(response.content)
         img=Image.open(image_data)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)#размер картинки в окне и чтобы качество не пострадало
+
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f'Произошла ошибка:{e}')
@@ -25,6 +27,10 @@ def set_image():
         label.image = img  # эта строчка не даст сборщику мусора питона удалить картинку
 
 
+def exit():
+    window.destroy()
+
+
 window=Tk()
 window.title('Собачки')
 window.geometry('600x480')
@@ -32,8 +38,16 @@ window.geometry('600x480')
 label=Label()
 label.pack()
 
-update_button=Button(text='Обновить', command=set_image)
-update_button.pack()
+# update_button=Button(text='Обновить', command=set_image)
+# update_button.pack()
+
+menu_bar=Menu(window)
+window.config(menu=menu_bar)
+file_menu=Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='Файл', menu=file_menu)
+file_menu.add_command(label='Загрузить фото', command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label='Выход',command=exit)
 
 url="https://dog.ceo/api/breeds/image/random"
 set_image()
