8ad90f5bc19ed9d6c5a9451871b2b7f809121c21
tatanakondakova <tnea@mail.ru>
2024-09-07 11:19:00 +0800

    Тело проекта

---
+++ dogs.py
@@ -0,0 +1,46 @@
+import requests
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox as mb
+from PIL import Image, ImageTk
+from io import BytesIO
+
+from bottle import response
+
+
+def get_random_dog_image():
+    try:
+        response=requests.get("https://dog.ceo/api/breeds/image/random")
+        response.raise_for_status()
+        data=response.json()
+        return data['message']
+    except Exception as e:
+        mb.showerror('Ошибка',f'Ошибка при запросе к API:{e}')
+
+
+def show_image():
+    image_url=get_random_dog_image()
+    if image_url:
+        try:
+            response=requests.get(image_url, stream=True)
+            response.raise_for_status()
+            img_data=BytesIO(response.content)
+            img=Image.open(img_data)
+            img.thumbnail((300,300))
+            img=ImageTk.PhotoImage(img)
+            label.config(image=img)
+            label.img=img
+        except requests.RequestException as e:
+            mb.showerror('Ошибка',f'Не удалось загрузить изображение:{e}')
+
+window = Tk()
+window.title('Собачки')
+window.geometry('360x420')
+
+label=Label()
+label.pack(padx=10, pady=10)
+
+button=Button(text='Загрузить изображение',command=show_image)
+button.pack(padx=10,pady=10)
+
+window.mainloop()
