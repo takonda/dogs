cac79fbebac8a7920c4a00fde268091be516359f
tatanakondakova <tnea@mail.ru>
2024-09-07 13:16:26 +0800

    Добавлена кнопка для загрузки нового изображения

--- dogs_2.py
+++ dogs_2.py
@@ -3,8 +3,10 @@ from tkinter import *
 from PIL import Image, ImageTk
 from io import BytesIO
 
+from pygame.display import update
 
-def load_image():
+
+def load_image(url):
     try:
         response = requests.get(url)# запрошенное отправить в response
         response.raise_for_status() # для обработки исключений
@@ -16,17 +18,24 @@ def load_image():
         return None
 
 
+def set_image():
+    img = load_image(url)
+    if img:
+        label.config(image=img)  # установим картинку на метку
+        label.image = img  # эта строчка не даст сборщику мусора питона удалить картинку
+
+
 window=Tk()
 window.title('Собачки')
 window.geometry('600x480')
 
 label=Label()
-label.pack(padx=10, pady=10)
+label.pack()
+
+update_button=Button(text='Обновить', command=set_image)
+update_button.pack()
 
 url="https://dog.ceo/api/breeds/image/random"
-img=load_image(url)
-if img:
-    label.config(image=img)#установим картинку на метку
-    label.image=img# эта строчка не даст сборщику мусора питона удалить картинку
+set_image()
 
 window.mainloop()
